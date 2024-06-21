8474058998103e1a288d6799d5fb748f2a45af5b
Thiago Marim <thiago.marim2005@gmail.com>
2024-06-21 19:53:50 -0300

    feat: add the function to create a new task

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import TasksCard from "./components/tasks-card";
 export interface TasksProps {
   id: string;
   text: string;
-  isMarked: boolean;
 }
 
 export default function App() {
@@ -14,35 +13,50 @@ export default function App() {
     {
       id: "0",
       text: "Correr",
-      isMarked: false,
     },
     {
       id: "1",
       text: "Correr 2",
-      isMarked: false,
     },
   ]);
+  const [input, setInput] = useState("");
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
+
+    setTasks([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        text: input,
+      },
+    ]);
+
+    setInput("");
   }
 
   return (
     <section className="container flex flex-col items-center justify-center w-full h-screen">
       <form onSubmit={handleSubmit} className="flex items-center w-full gap-2">
-        <Input placeholder="Escreva uma tarefa aqui!" className="h-12" />
+        <Input
+          onChange={(event) => setInput(event.target.value)}
+          placeholder="Escreva uma tarefa aqui!"
+          className="h-12"
+          required
+          value={input}
+        />
         <Button className="h-12">Enviar</Button>
       </form>
 
       <div className="mt-12 flex items-center gap-2 w-full">
         <span>Tarefas criadas</span>
         <div className="flex items-center justify-center size-6 bg-zinc-100 rounded-full">
-          <span className="text-sm">0</span>
+          <span className="text-sm">{tasks.length}</span>
         </div>
       </div>
 
       <div className="mt-6 w-full">
-        <TasksCard />
+        <TasksCard tasks={tasks} />
       </div>
     </section>
   );
--- src/components/tasks-card.tsx
+++ src/components/tasks-card.tsx
@@ -1,17 +1,23 @@
-import { FaRegCircle } from "react-icons/fa";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { TasksProps } from "../App";
 
-export default function TasksCard() {
+interface TasksCardProps {
+  tasks: TasksProps[];
+}
+
+export default function TasksCard({ tasks }: TasksCardProps) {
   return (
-    <section className="flex flex-col gap-12">
-      <div className="border-t border-zinc-300">
-        <div className="mt-4 flex items-center justify-between">
-          <p className="font-semibold">Correr</p>
-          <button className="flex items-center justify-center size-8 rounded-sm transition-colors bg-transparent hover:bg-red-200">
-            <RiDeleteBinLine size={20} color="#ef4444" />
-          </button>
+    <section className="flex flex-col gap-8">
+      {tasks.map((task) => (
+        <div key={task.id} className="border-b border-zinc-300">
+          <div className="mb-2 flex items-center justify-between">
+            <p className="font-semibold">{task.text}</p>
+            <button className="flex items-center justify-center size-8 rounded-sm transition-colors bg-transparent hover:bg-red-200">
+              <RiDeleteBinLine size={20} color="#ef4444" />
+            </button>
+          </div>
         </div>
-      </div>
+      ))}
     </section>
   );
 }

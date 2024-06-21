49764ec86f028b2b38dbabe188eea8c887ff912a
Thiago Marim <thiago.marim2005@gmail.com>
2024-06-21 19:35:48 -0300

    feat: add tasks-card function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,49 @@
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+import { FormEvent, useState } from "react";
+import TasksCard from "./components/tasks-card";
+
+export interface TasksProps {
+  id: string;
+  text: string;
+  isMarked: boolean;
+}
+
 export default function App() {
-  return <h1>Hello World!</h1>;
+  const [tasks, setTasks] = useState<TasksProps[]>([
+    {
+      id: "0",
+      text: "Correr",
+      isMarked: false,
+    },
+    {
+      id: "1",
+      text: "Correr 2",
+      isMarked: false,
+    },
+  ]);
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+
+  return (
+    <section className="container flex flex-col items-center justify-center w-full h-screen">
+      <form onSubmit={handleSubmit} className="flex items-center w-full gap-2">
+        <Input placeholder="Escreva uma tarefa aqui!" className="h-12" />
+        <Button className="h-12">Enviar</Button>
+      </form>
+
+      <div className="mt-12 flex items-center gap-2 w-full">
+        <span>Tarefas criadas</span>
+        <div className="flex items-center justify-center size-6 bg-zinc-100 rounded-full">
+          <span className="text-sm">0</span>
+        </div>
+      </div>
+
+      <div className="mt-6 w-full">
+        <TasksCard />
+      </div>
+    </section>
+  );
 }
--- src/components/tasks-card.tsx
+++ src/components/tasks-card.tsx
@@ -1,3 +1,17 @@
+import { FaRegCircle } from "react-icons/fa";
+import { RiDeleteBinLine } from "react-icons/ri";
+
 export default function TasksCard() {
-  return <div></div>;
+  return (
+    <section className="flex flex-col gap-12">
+      <div className="border-t border-zinc-300">
+        <div className="mt-4 flex items-center justify-between">
+          <p className="font-semibold">Correr</p>
+          <button className="flex items-center justify-center size-8 rounded-sm transition-colors bg-transparent hover:bg-red-200">
+            <RiDeleteBinLine size={20} color="#ef4444" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
